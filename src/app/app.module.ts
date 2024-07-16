23e364c4ceefe08a65a2f84a0835eb84fabbbc73
steven20012500 <steven20012500@gmail.com>
2024-07-16 09:10:33 -0500

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    //provideClientHydration()
   ],
   bootstrap: [AppComponent]
 })
