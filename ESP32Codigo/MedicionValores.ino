524115cf34a0f16059a7e8ea8fd50390b05fcbd0
steven20012500 <steven20012500@gmail.com>
2024-07-21 11:14:18 -0500

    sensoresSeguridad

--- ESP32Codigo/MedicionValores.ino
+++ ESP32Codigo/MedicionValores.ino
@@ -6,11 +6,15 @@
 #include "DHTesp.h"
 
 //credemciales Wifi
-const char* ssid = "*****";
-const char* password = "****";
+const char* ssid = "******";
+const char* password = "*******";
 
+const char* loginUrl = "http://ec2-18-191-140-37.us-east-2.compute.amazonaws.com:3000/api/login"; 
 const char* serverUrl = "http://ec2-18-191-140-37.us-east-2.compute.amazonaws.com:3000/api/bmp280";
 const char* dht22ServerUrl = "http://ec2-18-191-140-37.us-east-2.compute.amazonaws.com:3000/api/dht22";
+
+const char* username = "******"; 
+const char* passwordU = "****"; 
 
 #define BMP_SCK  (13)
 #define BMP_MISO (12)
@@ -19,10 +23,12 @@ const char* dht22ServerUrl = "http://ec2-18-191-140-37.us-east-2.compute.amazona
 
 Adafruit_BMP280 bmp; // I2C
 DHTesp dht; 
+String authToken; // Variable para almacenar el token
 
 void setup() {
   Serial.begin(9600);
   delay(1000);
+
    // Inicializar conexión WiFi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -30,6 +36,14 @@ void setup() {
     Serial.println("Connecting to WiFi...");
   }
   Serial.println("Connected to WiFi");
+
+  //Iniciar sesión y obtener token
+   if (login()) {
+    Serial.println("Login successful.");
+  } else {
+    Serial.println("Login failed.");
+    while (1) delay(10); // Detener el código si el login falla
+  }
 
   while (!Serial) delay(100);   // wait for native USB
   Serial.println(F("BMP280 test"));
@@ -59,6 +73,44 @@ void setup() {
   dht.setup(15, DHTesp::DHT22);
 }
 
+bool login(){
+  // Inicializar cliente HTTP
+  HTTPClient http;
+  http.begin(loginUrl);
+  http.addHeader("Content-Type", "application/json");
+
+  // Crear el JSON para la solicitud de login
+  String loginData = String("{\"username\":\"") + username +
+                     String("\",\"password\":\"") + passwordU + 
+                     String("\"}");
+
+  // Realizar la solicitud POST de inicio de sesión
+  int httpResponseCode = http.POST(loginData);
+
+  if (httpResponseCode > 0) {
+    String payload = http.getString();
+    Serial.println("Login response payload: " + payload);
+    
+    // Extraer el token del payload (suponiendo que el token está en el campo "token")
+    int tokenStart = payload.indexOf("token\":\"") + 8;
+    int tokenEnd = payload.indexOf("\"", tokenStart);
+    if (tokenStart > 8 && tokenEnd > tokenStart) {
+      authToken = payload.substring(tokenStart, tokenEnd);
+      Serial.println("Token received: " + authToken);
+      http.end();
+      return true;
+    } else {
+      Serial.println("Token not found in response.");
+    }
+  } else {
+    Serial.print("Error in HTTP request: ");
+    Serial.println(httpResponseCode);
+  }
+  http.end();
+  return false;
+}
+
+
 void loop() {
   float temperature = bmp.readTemperature();
   float pressure = bmp.readPressure();
@@ -71,11 +123,13 @@ void loop() {
   HTTPClient bmpHttp;
   bmpHttp.begin(serverUrl);
   bmpHttp.addHeader("Content-Type", "application/json");
+  bmpHttp.addHeader("Authorization", "Bearer " + authToken); // Agregar el token a la cabecera
+
   // Realizar solicitud POST al servidor
   int bmpHttpResponseCode  = bmpHttp.POST(bmpJsonData);
 
    if (bmpHttpResponseCode  > 0) {
-    Serial.print("HTTP Response code: ");
+    Serial.print("HTTP Response code BMP280: ");
     Serial.println(bmpHttpResponseCode);
     String bmpPayload  = bmpHttp.getString();
     Serial.println(bmpPayload);
@@ -98,6 +152,7 @@ void loop() {
   HTTPClient dhtHttp;
   dhtHttp.begin(dht22ServerUrl);
   dhtHttp.addHeader("Content-Type", "application/json");
+  dhtHttp.addHeader("Authorization", "Bearer " + authToken); // Agregar el token a la cabecera
 
   // Realizar solicitud POST al servidor para DHT22
   int dhtHttpResponseCode = dhtHttp.POST(dhtJsonData);
@@ -114,22 +169,22 @@ void loop() {
 
   dhtHttp.end();
 
-  // Print temperature in Celsius
+  // Imprimir temperatura en Celsius
   Serial.print(F("Temperature = "));
   Serial.print(bmp.readTemperature());
   Serial.println(F(" *C"));
 
-  // Print pressure in Pascals
+  // Imprimir altitud en metros basada en la presión local
   Serial.print(F("Pressure = "));
   Serial.print(bmp.readPressure());
   Serial.println(F(" Pa"));
 
   // Print altitude in meters based on local pressure
   Serial.print(F("Approx altitude = "));
-  Serial.print(bmp.readAltitude(1011.9)); /* Adjusted to local forecast! */
+  Serial.print(bmp.readAltitude(1011.9));  /* Ajustada a la previsión local! */
   Serial.println(F(" m"));
 
-  Serial.println(); // Print a blank line for readability
+  Serial.println();  // Imprimir una línea en blanco para legibilidad
 
    TempAndHumidity data = dht.getTempAndHumidity();
   //Mostramos los datos de la temperatura y humedad
@@ -137,7 +192,7 @@ void loop() {
   Serial.println("Humedad: " + String(data.humidity, 1) + "%");
   Serial.println("---");
   delay(1000);
-  Serial.println(); // Print a blank line for readability
+  Serial.println(); // Imprimir una línea en blanco para legibilidad
 
   delay(5000); // Esperar 5 segundos antes de enviar el siguiente dato
 }
