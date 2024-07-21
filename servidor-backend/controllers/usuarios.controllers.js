4a14f8f47dc052b69d3e48ebe30d0ce4d866029a
steven20012500 <steven20012500@gmail.com>
2024-07-21 12:13:07 -0500

    password

--- servidor-backend/controllers/usuarios.controllers.js
+++ servidor-backend/controllers/usuarios.controllers.js
@@ -61,10 +61,10 @@ usuariosController.addUsuario = async (req, res) => {
 }
 
 usuariosController.updatePassword = async (req, res) => {
-  const { username, oldPassword, newPassword } = req.body;
-
+  const { oldPassword, newPassword } = req.body;
+  const _id = req.userId;
   try {
-    const usuario = await Usuario.findOne({ username });
+    const usuario = await Usuario.findOne({ _id });
     if (!usuario) {
       return res.status(400).json({ msg: 'Usuario no encontrado' });
     }
@@ -138,19 +138,6 @@ usuariosController.getUserByUsername = async (req, res) => {
   }
 };
 
-usuariosController.getUserById = async (req, res) => {
-  const { _id } = req.userId ;
-
-  try {
-    const usuario = await Usuario.findById(_id);
-    if (!usuario) {
-      return res.status(404).json({ msg: 'Usuario no encontrado' });
-    }
-    res.json(usuario);
-  } catch (error) {
-    res.status(500).json({ message: 'Error interno en el servidor', error: error.message });
-  }
-}
 
 module.exports = usuariosController;
 
