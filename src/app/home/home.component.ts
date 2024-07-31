1da4f05b9e284f6e451d23118491eb2856eff610
klvallejo1 <leandrohernandez1903@gmail.com>
2024-07-30 21:43:05 -0500

    Responsive for Ipad Mini

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,8 +5,20 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  bmp280Flip = false;
+  dht22Flip = false;
+  esp32Flip = false;
 
+  flipCard(sensor: string) {
+    if (sensor === 'bmp280') {
+      this.bmp280Flip = !this.bmp280Flip;
+    } else if (sensor === 'dht22') {
+      this.dht22Flip = !this.dht22Flip;
+    } else if (sensor === 'esp32') {
+      this.esp32Flip = !this.esp32Flip;
+    }
+  }
 }
